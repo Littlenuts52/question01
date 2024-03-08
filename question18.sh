fdc8f25c92c9b87f493a5ae2cc1a7a63f7d64f21
赵梓博 <zhaozibo@zhaozibodeMacBook-Pro.local>
2024-03-08 21:50:30 +1030

    modify q18

---
+++ question18.sh
@@ -0,0 +1,14 @@
+git branch -a
+git checkout ready1
+git checkout ready2
+git checkout ready3
+git checkout main
+git merge ready1
+git merge ready2
+git merge ready3
+git branch -d ready1 ready2 ready3 
+git checkout update1
+git merge main
+git checkout update2
+git merge main
+
